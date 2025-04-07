1df0aa7fc94678e1df7fc32a183594b26b5db603
Larieli Vitoria <larielivitoria@gmail.com>
2025-04-07 10:10:06 -0300

    Adicionar arquivos de projeto.

---
+++ praticando if else/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Informe a nota final do aluno:");
+
+double notaFinal = double.Parse(Console.ReadLine());
+
+if (notaFinal >= 9)
+{
+    Console.WriteLine("você tirou A");
+}if (notaFinal >= 7 && notaFinal < 9)
+{
+    Console.WriteLine("Você tirou B");
+}
+if (notaFinal >= 5 && notaFinal < 7)
+{
+    Console.WriteLine("Você tirou C");
+}
+if (notaFinal < 5)
+{
+    Console.WriteLine("Você tirou D");
+}
